1682eb82c4ede2c4d59aa8ae962acfcb9d6ecfa8
LukezicA18 <49947160+LukezicA18@users.noreply.github.com>
2022-04-10 20:45:21 +0200

    Add files via upload
    popravki

--- seminarska.py
+++ seminarska.py
@@ -1,20 +1,22 @@
 import tweepy
 import pandas as pd
 import numpy as np
-# TwetAnalyzer
+import re
+import string  # to get all letters
 
 from openpyxl.workbook import Workbook
-
-from textblob import TextBlob   ##za dobre/slabe tweete
-import re   ## regularni izrazi, da dobimo ven besede
+from textblob import TextBlob     ## for positive/negative tweets
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 
+## Twitter keys
 api_key = "YoLSoQOTOeZk45dpIxxyn8HUN"
 api_secrets = "a7g4sCZeUmo6bXEyJpoOJiEIRyYWhuBl81UrJaLSmMkKhxrC6K"
 access_token = "1379169625011859459-eko5LNMGyH32NyHnB3QpnyJxc6Mdka"
 access_secret = "Ybazl2RrrboyCKzNjdgmbYqZKHAoQEBF9ssaTIRWjEBeL"
  
+
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(api_key,api_secrets)
 auth.set_access_token(access_token,access_secret)
@@ -22,27 +24,37 @@ auth.set_access_token(access_token,access_secret)
 api = tweepy.API(auth, wait_on_rate_limit = True)
 
 
-#tweepy.Cursor()  ##da ni limita 200
 
+## functions that we will need
 
-## funkciji ki nam pomagata povedati ali je tweet dober ali slab
 def clean_tweet(tweet):
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 def analyze_sentiment(tweet):
+    '''To see if the tweet is positive, negative or neutral.'''
     analysis = TextBlob(clean_tweet(tweet))
         
     if analysis.sentiment.polarity > 0:
-        return 1
+        return 'positive'
     elif analysis.sentiment.polarity == 0:
-        return 0
+        return 'neutral'
     else:
-        return -1
+        return 'negative'
+
+def polarity(tweet):
+    '''To get polarity of the tweet.'''
+    analysis = TextBlob(clean_tweet(tweet))
+    return round(analysis.sentiment.polarity, 3)
+
+def subjectivity(tweet):
+    '''To get polarity of the tweet.'''
+    analysis = TextBlob(clean_tweet(tweet))
+    return round(analysis.sentiment.subjectivity, 3)
 
 
-# ## po različnih userjih
-# users = ['elonmusk', 'TheNotoriousMMA', '3gerardpique', 'VancityReynolds', 'FCBarcelona', 'premierleague', 'joerogan']
-# limit = 10
+### if we want to see tweets according to diferent users
+# users = list(input('Tweets of wich users do you want to analyze? '))
+# limit = int(input('How many tweets of one user do you want to analyze? '))
 # columns = ['Time', 'User', 'Tweet', 'Followers', 'Retweets', 'Likes']
 # data = []
 # 
@@ -58,29 +70,48 @@ def analyze_sentiment(tweet):
 # df = pd.DataFrame(data, columns = columns)
 # # print(df)
 
-## ce iscemo tweete po neki besedi
-keywords = 'Putin'
-limit = 200
+
+## if we want to see tweets that contain a specific word/hashtag
+keyword = input('Tweets with wich word/hashtag do you want to analyze? ')
+limit = int(input('How many tweets do you want to analyze? ')) 
 columns = ['Time', 'User', 'Tweet', 'Retweets']
 data = []
 
 
-tweets = tweepy.Cursor(api.search_tweets, q = keywords, count = 200, tweet_mode = 'extended').items(limit)
+tweets = tweepy.Cursor(api.search_tweets, q = keyword, count = 200, tweet_mode = 'extended').items(limit)
+# Cursor: that we can see more tweets
 for tweet in tweets:
-    if tweet.lang == 'en':   ## ce tweet ni v anglescini ga izlocimo
+    if tweet.lang == 'en':   ## if tweet is not in english, delete it
         data.append([tweet.created_at, tweet.user.screen_name, tweet.full_text, tweet.retweet_count])
     
-pd.set_option('display.max_rows', None)  ## da pokaze vse vrstice
-pd.set_option('display.max_columns', None)   ## da pokaze vse stolpce
+pd.set_option('display.max_rows', None)  ## that wee can see all rows
+pd.set_option('display.max_columns', None)   ## that we can se all columns
 df = pd.DataFrame(data, columns = columns)
 # print(df)
 
-## pogledamo kaksni so bili tweeti (pozitivni/nevtralni/negativni)
+## to see if tweet is positivei/neutral/negative
 sentiments = []
 for sentiment in df['Tweet']:
     sentiments.append(analyze_sentiment(sentiment))
     
 df['Sentiment'] = sentiments
+
+
+## polarity
+polarities = []
+for text in df['Tweet']:
+    polarities.append(polarity(text))
+    
+df['Polarity'] = polarities
+
+
+## subjectivity
+subjectivities = []
+for text in df['Tweet']:
+    subjectivities.append(subjectivity(text))
+    
+df['Subjectivity'] = subjectivities
+
 
 
 ## pogledamo koliko znakov imajo tweeti
@@ -90,25 +121,49 @@ for text in df['Tweet']:
 
 df['Length'] = lengths
 
+
 #pd.set_option('display.max_columns', None)
 
 #df.to_csv('poskus.csv')
-#print(df)
+print(df)
 
 
 
-## Poglejmo povprečno dolžino
-print(f"Average length: {round(np.mean(df['Length']), 1)}")
+## Average polarity of tweets
+print(f"Average length: {np.mean(df['Polarity'])}")
 
-
-## Največ retweetov
+## Average subjectivity of tweets
+print(f"Average length: {np.mean(df['Subjectivity'])}")
+# 
+## Most reetweets
 print(f"Most reetwets: {np.max(df['Retweets'])}")
 
 
 
+## The most positive and the most negative tweet
+
+print('\n')
+print('The most positive tweet (one of): ')
+for i in range(len(df['Polarity'])):
+    if df['Polarity'][i] == np.max(df['Polarity']):
+        print(df['Tweet'][i])
+        print('\n')
+        break     # if more tweets have the same polarity we print just one
+
+print('The most negative tweet (one of): ')
+for i in range(len(df['Polarity'])):
+    if df['Polarity'][i] == np.min(df['Polarity']):
+        print(df['Tweet'][i])
+        print('\n')
+        break
+
+
+
+
+
 #Time Series
-time_likes = pd.Series(data=df['Retweets'].values, index=df['User'])
-time_likes.plot(figsize=(16, 4), color='r')
+# time_likes = pd.Series(data=df['Polarity'].values, index=df['Time'])
+# time_likes.plot(figsize=(16, 4), color='r')
 # plt.show()
     
 #time_favs = pd.Series(data=df['likes'].values, index=df['date'])
@@ -135,215 +190,83 @@ neu_tweets = []
 
 #picking positive tweets from tweets
 for i in range(len(df['Sentiment'])):
-    if df['Sentiment'][i] == 1:
+    if df['Sentiment'][i] == 'positive':
         pos_tweets.append(df['Tweet'][i])
 # percentage of positive tweets
-positive = round(100*len(pos_tweets)/len(df['Sentiment']), 1)  #procent pozitivnih tweetov
+positive = round(100 * len(pos_tweets) / len(df['Sentiment']), 1)  #percentage of positive tweets
 print("Positive tweets percentage: {} %".format(positive))
+
 # picking negative tweets from tweets
 for i in range(len(df['Sentiment'])):
-    if df['Sentiment'][i] == -1:
+    if df['Sentiment'][i] == 'negative':
         neg_tweets.append(df['Tweet'][i])
 # percentage of negative tweets
-negative = round(100*len(neg_tweets)/len(df['Sentiment']), 1)   ## procent negativnih tweetov
+negative = round(100 * len(neg_tweets) / len(df['Sentiment']), 1)   #percentage of negative tweets
 print("Negative tweets percentage: {} %".format(negative))
 # percentage of neutral tweets
-neutral = round(100*(len(df['Sentiment']) - (len(neg_tweets)+len(pos_tweets)))/len(df['Sentiment']), 1)  ##procent nevtralnih
+for i in range(len(df['Sentiment'])):
+    if df['Sentiment'][i] == 'neutral':
+        neu_tweets.append(df['Tweet'][i])
+#neutral = round(100 * (len(df['Sentiment']) - (len(neg_tweets)+len(pos_tweets)))/len(df['Sentiment']), 1) 
+neutral = round(100 * len(neu_tweets) / len(df['Sentiment']), 1)   #percentage of neutral tweets
 print("Neutral tweets percentage: {} %".format(neutral))
-#   
-## printing first 10 positive tweets
-# print("\n\nPositive tweets:")
-# for tweet in pos_tweets[:10]:
+
+# ## printing first 5 positive tweets
+# print("\nPositive tweets:")
+# for tweet in pos_tweets[:5]:
 #     print(tweet)
     
     
-#Creating PieCart
-labels = ['Positive ['+str(positive)+'%]' , 'Neutral ['+str(neutral)+'%]','Negative ['+str(negative)+'%]']
-sizes = [positive, neutral, negative]
-colors = ['green', 'blue','red']
-#patches, texts =
-#colors=colors
-plt.pie(sizes, labels = labels, colors = colors)  ## lahko dodamo explode = [0.2, 0, 0, 0] da bo en odmaknjen, isto lahko startangle = 90, da zacne zgoraj
-#plt.style.use('default')
-#plt.legend(labels) ##nekaj ni prav
-plt.title('Sentiment Analysis')
-#plt.axis('equal')
+
+# Histogram
+array_podatkov = np.array([df['Polarity'][i] for i in range(len(df['Polarity']))])
+plt.hist(array_podatkov)
+plt.xlabel("Sentiment")
+plt.ylabel("Number of tweets")
+plt.title(f"Sentiment Analysis")
+plt.xticks(np.arange(-1, 1, 0.25))
+plt.xlim([-1, 1])
 plt.show()
 
 
 
 
+#Creating PieCart
+marks = ['Positive (' + str(positive) + '%)' , 'Neutral (' + str(neutral) + '%)','Negative (' + str(negative) + '%)']
+sizes = [positive, neutral, negative]
+colors = ['green', 'orange', 'red']
+plt.pie(sizes, labels = marks, colors = colors)  ## lahko dodamo explode = [0.2, 0, 0, 0] da bo en odmaknjen, isto lahko startangle = 90, da zacne zgoraj
+plt.title('Sentiment Analysis')
+plt.show()
+
+
+
+## Word Cloud
+text = " ".join(tweet for tweet in df['Tweet'])
+text = clean_tweet(text)
+
+letters = list(string.ascii_lowercase)
+
+stop_words = ['RT', 'https', 'co', 're']  + list(STOPWORDS) + letters # že vgrjenim besedam, ki jih izbriše dodamo še RT, ker pomeni reetweet. s zato ker je okrajšava za is, was...
+wordcloud = WordCloud(stopwords = stop_words, background_color="white").generate(text)  #stopwords=stop_words da se znebi kaksnih besed
+plt.imshow(wordcloud)
+plt.axis("off")    ## ne pokaže vrednosti (x, y os)
+plt.show()
 
 
 
 
+##Let's see if there is a connection between polarity and subjectivity
+x = np.array(df['Subjectivity'])
+y = np.array(abs(df['Polarity']))  ##abs vrednost zato da se lepše vidi ali so bolj nevtralni tweeti tudi bolj subjektivni
 
+x0, y0 = np.polyfit(x, y, 1) #find the line that fits best
+plt.scatter(x, y)  #draw a plot with points
+plt.plot(x, x0 * x + y0)  #add line that fits best to the plot
 
+plt.xlabel("Subjectivity")
+plt.ylabel("Polarity")
 
+plt.show()
 
-# ## funkciji ki nam pomagata povedati ali je tweet dober ali slab
-# def clean_tweet(tweet):
-#     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
-# 
-# def analyze_sentiment(tweet):
-#     analysis = TextBlob(clean_tweet(tweet))
-#         
-#     if analysis.sentiment.polarity > 0:
-#         return 1
-#     elif analysis.sentiment.polarity == 0:
-#         return 0
-#     else:
-#         return -1
-    
-    
-## ce iscemo tweete po neki besedi
-    
-# keywords = 'Putin'
-# limit = 50
-# columns = ['User', 'Tweet']
-# data = []
-# 
-# 
-# tweets = api.search_tweets(q = keywords, count = limit, tweet_mode = 'extended')
-# for tweet in tweets:
-#     data.append([tweet.user.screen_name, tweet.full_text])
-# 
-# df = pd.DataFrame(data, columns = columns)
-# # print(df)
-# 
-# 
-# ## koda nam pove ali je tweet pozitiven ali negativen
-# tweet_analyzer = TweetAnalyzer()
-# data1 = []
-# for a in df['Tweet']:
-#     data1.append(analyze_sentiment(a))
-#     
-# sent = pd.DataFrame(data1, columns = ['Sentiment'])
-# print(df.append(sent).head(50))
-
-
-#print(df['Tweet'])
-
-# tweet_analyzer = TweetAnalyzer()
-
-# df = tweet_analyzer.df
-# df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
-
-
-
-
-
-
-
-# columns = ['Time', 'Tweet', 'User']
-# data = []
-# for tweet in public_tweets:
-#     data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
-# 
-# df = pd.DataFrame(data, columns = columns)
-
-
-# user = 'elonmusk'
-# limit = 100
-
-# tweets = api.user_timeline(id='elonmusk', count=200)
-# tweets_extended = api.user_timeline(id='elonmusk', tweet_mode='extended', count=200)
-#  
-# # Print number of tweets
-# print(len(tweets))
-
-# tweets = api.user_timeline(screen_name = user, count = limit, tweet_mode = 'extended')
-# 
-# columns = ['User', 'Tweet']
-# data = []
-# 
-# for tweet in tweets:
-#     data.append([tweet.user.screen_name, tweet.full_text])
-#     
-# df = pd.DataFrame(data, columns = columns)
-# print(df)
-
-
-# columns = ['Time', 'User', 'Tweet']
-# data = []
-# for tweet in public_tweets:
-#     data.append([tweet.created_at, tweet.user.screen_name, tweet.text])
-# 
-# df = pd.DataFrame(data, columns = columns)
-# 
-# df.to_csv('tweets.csv')
-
-# ### Tweets
-#  
-# tweets = api.user_timeline(id='Aljaz32412310', count=10)
-# tweets_extended = api.user_timeline(id='Aljaz32412310', tweet_mode='extended', count=10)
-#  
-# # Print number of tweets
-# print(tweets)
-# #200
-# 
-# to_extract = [
-#     'id',
-#     'created_at',
-#     'text',
-#     'full_text',
-# #     'retweeted',
-# #     'favorited',
-# #     'is_quote_status',
-# #     'retweet_count',
-# #     'favorite_count',
-# #     'lang',
-# #     'in_reply_to_status_id',
-# #     'in_reply_to_user_id'
-#     ]
-#  
-# tweet_entities = [
-#     ('hashtags','text'),
-#     ('user_mentions','screen_name'),
-#     ('urls','expanded_url')
-# ]
-# tweets_data = []
-#  
-# for tweet in tweets_extended:
-#     tweet = tweet._json
-#     tweet_data = {}
-#     for e in to_extract:
-#         try:
-#             tweet_data[e]=tweet[e]
-#         except:
-#             continue
-#     for entity in tweet_entities:
-#         entity_list = []
-#         for t in tweet['entities'][entity[0]]:
-#             entity_list.append(t[entity[1]])
-#         tweet_data[entity[0]] = entity_list
-#  
-#     tweets_data.append(tweet_data)
-#     # Get Tweet data
-# df = pd.DataFrame(tweets_data)
-
-# user = api.get_user('Aljaz32412310')
-# user._json
-# print(f"user.name: {user.name}")
-# print(f"user.screen_name: {user.screen_name}")
-# print(f"user.location: {user.location}")
-# print(f"user.description: {user.description}")
-# print(f"user.followers_count: {user.followers_count}")
-# print(f"user.listed_count: {user.listed_count}")
-# print(f"user.statuses_count: {user.statuses_count}")
-# 
-# print('Latest Followers:')
-# for follower in user.followers():
-#     print('Name: ' + str(follower.name))
-#     print('Username: ' + str(follower.screen_name))
-# 
-# user = api.get_user('elonmusk')
-# user._json
-# print(f"user.name: {user.name}")
-# print(f"user.screen_name: {user.screen_name}")
-# print(f"user.location: {user.location}")
-# print(f"user.description: {user.description}")
-# print(f"user.followers_count: {user.followers_count}")
-# print(f"user.listed_count: {user.listed_count}")
-# print(f"user.statuses_count: {user.statuses_count}")
 
